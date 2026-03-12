6780902bbc095fe7bac8a861a57e139981d76a5a
German Robayo Paz <gerobayopaz@microsoft.com>
2026-03-11 18:34:13 -0700

    fix install script

--- install-appnet-private-preview.sh
+++ install-appnet-private-preview.sh
@@ -2,7 +2,10 @@
 
 set -x
 
-PRIVATE_PREVIEW_CLI_URL=https://github.com/german1608/azure-cli-extensions/raw/refs/heads/gerobayopaz/appnet-internal/appnet_private_preview-1.0.0b1-py3-none-any.whl
+# Get version from setup.py
+VERSION=$(curl -s https://raw.githubusercontent.com/german1608/azure-cli-extensions/gerobayopaz/appnet-internal/src/appnet-preview/setup.py | grep "VERSION = " | sed "s/VERSION = '//g" | sed "s/'//g")
+
+PRIVATE_PREVIEW_CLI_URL="https://github.com/german1608/azure-cli-extensions/raw/refs/heads/gerobayopaz/appnet-internal/appnet_private_preview-${VERSION}-py3-none-any.whl"
 
 # Get the final URL after following redirects
 final_url=$(curl -Ls -o /dev/null -w '%{url_effective}' "$PRIVATE_PREVIEW_CLI_URL")
